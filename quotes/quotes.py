b393fe721250ca53d3c34bd3ef6825e530c4d2d0
CiroBurro <166933083+CiroBurro@users.noreply.github.com>
2024-09-17 21:50:23 +0200

    Add files via upload

---
+++ quotes/quotes.py
@@ -0,0 +1,34 @@
+import json, random
+from googletrans import Translator
+from twilio.rest import Client
+
+traduttore = Translator()
+
+def load_quotes():
+    with open('citazioni.json', 'r', encoding='utf-8') as file:
+        citazioni = json.load(file)
+    return citazioni
+
+citazioni = load_quotes()
+random_quote = random.choice(citazioni)
+
+def traduci_citazione(citazione):
+    traduzione = traduttore.translate(citazione['quote'], src='en', dest='it')
+    return traduzione.text
+
+tradotta = traduci_citazione(random_quote)
+print(tradotta)
+
+account_sid = "ACf0a79eeb7e2a133f483c3e0bed5514e5"
+auth_token = "d8c717c37672ecf187741f8e87083679"
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  body=tradotta,
+  to='whatsapp:+393533641788'
+)
+
+print(message.sid)
+
+recovery_code = "JUK7LYMM28FYH7G63FV5KWFE"
